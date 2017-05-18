277a1082585bc3b1c66aaee37cd8df2112fd85f9
Quentin RIBAC <ribac.quentin@gmail.com>
2017-05-18 18:23:39 +0200

    42

--- src/XFacteur/MapView.java
+++ src/XFacteur/MapView.java
@@ -1,16 +1,25 @@
 package XFacteur;
 
 import javafx.scene.Group;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.GridPane;
 import javafx.scene.web.WebView;
+import javafx.scene.control.Button;
 
 public class MapView extends Group {
+	protected GridPane grid = new GridPane();
+	protected Button genBtn = new Button("Générer");
 	protected WebView wView = new WebView();
 
 	public MapView() {
-		this.getChildren().add(wView);
-		wView.getEngine().load("https://github.com/ribacq");
+		grid.setHgap(4);
+		grid.setVgap(8);
+		genBtn.setOnAction(e -> {
+			wView.getEngine().load("http://en.wikipedia.org/wiki/random");
+		});
+		wView.getEngine().load("http://lipsum.org");
+		grid.add(wView, 0, 1);
+		grid.add(genBtn, 0, 0);
+		this.getChildren().add(grid);
 	}
 }
 
