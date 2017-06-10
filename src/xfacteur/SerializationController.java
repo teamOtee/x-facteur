2b46b14b2fc701b3bb863208c2918eb25db39558
leomam <axel.froly@gmail.com>
2017-06-10 15:58:55 +0200

    bon la ça devrait être bon

--- src/xfacteur/SerializationController.java
+++ src/xfacteur/SerializationController.java
@@ -1,19 +1,13 @@
 package xfacteur;
 
-import javafx.collections.ObservableList;
-
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-import javafx.collections.FXCollections;
 import xfacteur.model.Mailman;
-import xfacteur.model.Path;
-import xfacteur.model.PathGenerator;
 import xfacteur.model.Shipment;
-import xfacteur.view.PathView;
 
 public class SerializationController {
 	// inaccessible constructor
