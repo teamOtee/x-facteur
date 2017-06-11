bbd584483c9ef534fe040d04637ed3eef1926549
Cleo Daguin <oelc@live.fr>
2017-06-11 17:15:11 +0200

    Add files for cereal but not over yet

---
+++ SerializationController.java
@@ -0,0 +1,113 @@
+package xfacteur;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import javafx.collections.ObservableList;
+import xfacteur.model.Mailman;
+import xfacteur.model.Shipment;
+
+public class SerializationController {
+	// inaccessible constructor
+	protected SerializationController() {
+	}
+
+	public static void serialization(String file1, String file2) {
+		// Output of Shipments
+		ObjectOutputStream oos = null;
+		try {
+			final FileOutputStream fileOut1 = new FileOutputStream(file1 + ".ship");
+			oos = new ObjectOutputStream(fileOut1);
+			oos.writeObject(ShipmentController.getItems());
+			oos.flush();
+		} catch (final java.io.IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (oos != null) {
+					oos.flush();
+					oos.close();
+				}
+			} catch (final IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		// Output of Mailmans
+
+		ObjectOutputStream oos2 = null;
+		try {
+			final FileOutputStream fileOut2 = new FileOutputStream(file2 + ".mail");
+			oos2 = new ObjectOutputStream(fileOut2);
+			for (Shipment s : ShipmentController.getItems()) {
+				oos2.writeObject(s);
+			}
+			oos2.writeObject(MailmanController.getItems());
+			oos2.flush();
+		} catch (final java.io.IOException e) {
+			System.out.println("I/O Exception while writing to file");
+			e.printStackTrace();
+		} finally {
+			try {
+				if (oos2 != null) {
+					oos2.flush();
+					oos2.close();
+				}
+			} catch (final IOException ex) {
+				System.out.println("I/O Exception while writing to file");
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	public static void deserializationS(String file1) {
+		// Input of Shipments
+		ObjectInputStream ois = null;
+		try {
+			final FileInputStream fileIn1 = new FileInputStream(file1);
+			ois = new ObjectInputStream(fileIn1);
+			ShipmentController.setItems((ObservableList<Shipment>) ois.readObject());
+		} catch (final java.io.IOException e) {
+			System.out.println("IO Exception while reading file");
+			e.printStackTrace();
+		} catch (final ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("File format is wrong");
+		} finally {
+			try {
+				if (ois != null) {
+					ois.close();
+				}
+			} catch (final IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+    }
+    
+    public static void deserializationM(String file2) {
+		// Input of Mailmans
+		ObjectInputStream ois2 = null;
+		try {
+			final FileInputStream fileIn2 = new FileInputStream(file2);
+			ois2 = new ObjectInputStream(fileIn2);
+			MailmanController.setItems((ObservableList<Mailman>) ois2.readObject());
+		} catch (final java.io.IOException e) {
+			e.printStackTrace();
+			System.out.println("IO Exception while reading file");
+		} catch (final ClassNotFoundException e) {
+			System.out.println("File format is wrong");
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ois2 != null) {
+					ois2.close();
+				}
+			} catch (final IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
---
+++ SerializationModal.java
@@ -0,0 +1,76 @@
+package xfacteur.view;
+
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.text.Text;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+
+import xfacteur.XFacteur;
+import xfacteur.model.Mailman;
+
+public class SeializationModal extends Stage {
+	protected GridPane gridP = new GridPane();
+	protected Text header = new Text("Veuillez remplir tous les champs sans extention:");
+	protected Label mailmanNameL = new Label("Nom du fichier des facteurs :");
+	protected Label shipmentNameL = new Label("Nom du fichier des envois :");
+	protected TextField mailmanName = new TextField();
+	protected TextField shipmentName = new TextField();
+	protected Button okBtn = new Button("Enregistrer");
+	protected Button cancelBtn = new Button("Annuler");
+
+	public MailmanEditModal() {
+		this.setTitle("Enregistrer les facteurs et les envois — X Facteur");
+		this.initModality(Modality.APPLICATION_MODAL);
+		this.getIcons().add(new Image(XFacteur.LOGOPATH));
+		this.setMaxWidth(300);
+		this.setMaxHeight(300);
+		this.setResizable(false);
+		this.setScene(new Scene(content()));
+		makeInteractivity();
+	}
+
+	protected GridPane content() {
+		gridP.setHgap(8);
+		gridP.setVgap(6);
+		gridP.setPadding(new Insets(10, 20, 12, 20));
+		gridP.add(header, 0, 0, 2, 1);
+		gridP.add(mailmanNameL, 0, 2);
+		gridP.add(mailmanName, 1, 2, 2, 1);
+		gridP.add(shipmentNameL, 0, 1);
+		gridP.add(shipmentName, 1, 1, 2, 1);
+		GridPane.setHalignment(okBtn, HPos.RIGHT);
+		GridPane.setHalignment(cancelBtn, HPos.RIGHT);
+		okBtn.setPrefWidth(80);
+		cancelBtn.setPrefWidth(80);
+		gridP.add(okBtn, 1, 3);
+		gridP.add(cancelBtn, 2, 3);
+
+		return gridP;
+	}
+
+	protected void makeInteractivity() {
+		okBtn.setDisable(true);
+		mailmanName.setOnKeyReleased(e -> {
+			okBtn.setDisable(mailmanName.getText().isEmpty() || shipmentName.getText().isEmpty());
+		});
+		shipmentName.setOnKeyReleased(e -> {
+			okBtn.setDisable(mailmanName.getText().isEmpty() || shipmentName.getText().isEmpty());
+		});
+		okBtn.setOnAction(e -> {
+			SerializationController.serialization(shipmentName.getText(),mailmanName.getText());
+			this.close();
+		});
+		cancelBtn.setOnAction(e -> {
+			this.close();
+		});
+	}
+
+}
